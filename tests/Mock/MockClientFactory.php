86ad8779dc626d5a91846cb44cc8fa3eddf555a1
Pavel Stejskal <spajxo@gmail.com>
2025-04-01 10:00:01 +0200

    Drop dependency on thecodingmachine/safe (#34)

--- tests/Mock/MockClientFactory.php
+++ tests/Mock/MockClientFactory.php
@@ -8,21 +8,34 @@ use Http\Message\RequestMatcher\RequestMatcher;
 use Http\Mock\Client;
 use Http\Mock\Client as MockClient;
 use Nyholm\Psr7\Response;
+use RuntimeException;
 
-use function Safe\file_get_contents;
+use function file_get_contents;
 
 final class MockClientFactory
 {
     public static function create(): Client
     {
         $mockClient = new MockClient();
+        $configuration = file_get_contents(TESTS_DIR . '/Mock/configuration.json');
+
+        if ($configuration === false) {
+            throw new RuntimeException('Failed to read configuration.json');
+        }
+
+        $jwks = file_get_contents(TESTS_DIR . '/Mock/jwks.json');
+
+        if ($jwks === false) {
+            throw new RuntimeException('Failed to read jwks.json');
+        }
+
         $mockClient->on(
             new RequestMatcher("/.well-known/openid-configuration"),
-            new Response(body: file_get_contents(TESTS_DIR . '/Mock/configuration.json')),
+            new Response(body: $configuration),
         );
         $mockClient->on(
             new RequestMatcher("/.well-known/jwks"),
-            new Response(body: file_get_contents(TESTS_DIR . '/Mock/jwks.json')),
+            new Response(body: $jwks),
         );
 
         return $mockClient;
